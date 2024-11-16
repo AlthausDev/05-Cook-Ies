f23d4a0461ebd6f1ef8d9f1c97d7772ae14a81c5
AlthausDev <samuelalthaus@gmail.com>
2024-11-16 17:21:39 +0100

    style: ajustado estilo del perfil de usuario

--- app/src/main/java/com/althaus/dev/cookIes/data/model/UserProfile.kt
+++ app/src/main/java/com/althaus/dev/cookIes/data/model/UserProfile.kt
@@ -8,9 +8,12 @@ data class UserProfile(
     @DocumentId val id: String = "",                  // ID del usuario en Firestore
     val name: String = "",                            // Nombre del usuario, predeterminado a una cadena vacía
     val email: String = "",                           // Email, predeterminado a una cadena vacía para evitar nulls
-    val profileImageUrl: String? = null,              // URL de la foto de perfil; nulo si no tiene una imagen
+    val profileImage: String? = null,                 // URL de la foto de perfil; nulo si no tiene una imagen
     val favorites: List<String> = emptyList(),        // Lista inmutable de IDs de recetas favoritas
     val bio: String = "",                             // Biografía del usuario
     val creationDate: Long = System.currentTimeMillis(), // Timestamp de creación de la cuenta
     val isVerified: Boolean = false                   // Indicador de cuenta verificada
-)
+
+) {
+
+}
